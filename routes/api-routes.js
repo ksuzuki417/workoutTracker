cd0345c42fe6f0444d03cfd6253b7f520f05724b
Keita Suzuki <ksuzuki417@gmail.com>
2020-11-16 23:57:42 -0600

    html-routes completed

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,13 @@
 const Workout = require ("../models/WorkoutDB");
 
 module.exports = function(app) {
-    
+    app.post("/api/workouts", (req, res) => {
+        Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,15 @@
 const path = require("path");
 
 module.exports = function(app) {
-    
-}
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/excercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/excercise.html"));
+    });
+
+    app.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+};
