e52fa781a6bbe65cf3ceb33c7380338e8fa6c52f
Keita Suzuki <ksuzuki417@gmail.com>
2020-11-17 00:07:20 -0600

    minor fixe on html-route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    app.get("/excercise", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/excercise.html"));
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
     app.get("/stats", function(req, res) {
