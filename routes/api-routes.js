d3ef1293363bf7006d002e0d8261770319c2219d
Keita Suzuki <ksuzuki417@gmail.com>
2020-11-18 01:31:17 -0600

    api routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,44 @@
 const Workout = require ("../models/WorkoutDB");
 
 module.exports = function(app) {
-    app.post("/api/workouts", (req, res) => {
+    app.post("/api/workouts", function(req, res) {
         Workout.create({})
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
         .catch(err => {
             res.json(err);
+        });
+    });
+
+    app.get("/api/workouts", function(req, res) {
+        Workout.find({}, function(error, data) {
+            if (error) {
+                res.send(error);
+            } else {
+                res.json(data);
+            }
+        });
+    });
+
+    app.put("/api/workouts/:id", function({ body, params}, res) {
+        Workout.findByIdAndUpdate(params.id, { $push: {exercises: body }},
+            {new: true, runValidators: true })
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            });
+    });
+
+    app.get("/api/workouts/range", function(req, res){
+        Workout.find({}.limit(10), (error, data) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.json(data);
+            }
         });
     });
 }
